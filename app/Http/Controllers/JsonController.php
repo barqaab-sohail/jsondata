c07bf0cd18e54f45d5a6ae74a564f4f5bac41d00
Sohail Afzal <sohail.afzal@barqaab.com>
2023-04-05 21:38:05 +0500

    05-04-2023

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -15,9 +15,15 @@ class JsonController extends Controller
         $extension = '';
         if ($request->hasFile('select_file')) {
             $extension = request()->select_file->getClientOriginalExtension();
+            $fileName =  time() . '.' . $extension;
+            $folderName = "json/";
+            $request->file('select_file')->storeAs('public/' . $folderName, $fileName);
+            $file_path = storage_path('app/public/' . $folderName . $fileName);
+            $data = file_get_contents($file_path); //data read from json file
+            $users = json_decode($data);
             // $data = file_get_contents($request->file('select_file'));
             // print_r($data);
         }
-        return response()->json(['message' => "Data saved successfully. $extension"]);
+        return response()->json(['message' => "Data saved successfully. $users"]);
     }
 }
